368303d6ccef7b893c8db89b3d78137e044a9072
Maria Del Rosario Leon Cure <ICF7663B@co.attla.corp>
2019-06-25 10:05:10 -0500

    Changes of flak config

--- WebkhookV1/webhook-v2.py
+++ WebkhookV1/webhook-v2.py
@@ -259,5 +259,5 @@ def encontrarValores():
 				servicioG = keyId
 				serviceCodeG = 'NULL'
 
-if __name__ == "__main__":
-	app.run(debug = True)
+if __name__ == '__main__':
+	app.run(host='127.0.0.1', port=8080, debug=True)
